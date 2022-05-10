6965e499c55541b760999cebdc52e30d21567ace
min2oyo <min2u@outlook.com>
2022-05-10 22:11:30 +0900

    firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjdPoiF_fotHVMNtY0RnL52GJCkHwZbuo",
+  authDomain: "clone-5fa0d.firebaseapp.com",
+  projectId: "clone-5fa0d",
+  storageBucket: "clone-5fa0d.appspot.com",
+  messagingSenderId: "246014457134",
+  appId: "1:246014457134:web:72ea4fc480510dab2d76bf",
+  measurementId: "G-HMZXVNFRF1"
+};
